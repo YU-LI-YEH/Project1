d8224c683850cff0b8d6d534ce38eb43477147cc
葉禹利 <kevin3517@gmail.com>
2020-10-07 23:12:40 +0800

    Pseudocode Project1.cpp

---
+++ Project1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+
+while (!touches the boundary){
+    cin pattern 
+    for (;the upper row are not all zero; column down)
+    shift left or right
+    if (row has all 1) {
+        clear the row 
+        all others 1 goes down 
+    }
+}
